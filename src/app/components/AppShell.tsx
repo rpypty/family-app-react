b880fbb1501b1c5eeecb8cc8c621a6f2b785061d
zhda13 <zhda13@bwg-io.site>
2026-08-23 16:46:37 +0300

    feat: show moved screen instead of expenses mini-app
    Расходы стали отдельным приложением «Купилка» на kupilka.site. Весь мини-апп
    расходов, на любой вкладке и по любой старой ссылке, показывает экран с
    объявлением и ссылкой на новый адрес; нижний таббар расходов убран.
    
    Todo и Тренировки не затронуты.

--- src/app/components/AppShell.tsx
+++ src/app/components/AppShell.tsx
@@ -1,7 +1,6 @@
 import { Container, Stack } from '@mui/material'
 import type { AppShellModel } from '../hooks/useAppController'
 import { AppFamilyDialog } from './shell/AppFamilyDialog'
-import { AppShellExpensesTabs } from './shell/AppShellExpensesTabs'
 import { AppShellHeader } from './shell/AppShellHeader'
 import { AppShellMainContent } from './shell/AppShellMainContent'
 import { AppShellMenu } from './shell/AppShellMenu'
@@ -19,14 +18,13 @@ export function AppShell({ model }: AppShellProps) {
       <AppShellMenu model={model} />
       <AppFamilyDialog model={model} />
 
-      <Container maxWidth="md" sx={{ pt: 2, pb: model.activeApp === 'expenses' ? 12 : 6 }}>
+      <Container maxWidth="md" sx={{ pt: 2, pb: 6 }}>
         <Stack spacing={3}>
           <AppShellSyncAlerts model={model} />
           <AppShellMainContent model={model} />
         </Stack>
       </Container>
 
-      <AppShellExpensesTabs model={model} />
       <AppShellOfflineSyncNotice model={model} />
     </>
   )
